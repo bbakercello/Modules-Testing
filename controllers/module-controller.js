6897cbe9097236dc5b35f5bdbd8d076e28c48d70
bbakercello <bbakercello@gmail.com>
2022-09-14 15:43:28 -0400

    initial backend complete

--- controllers/module-controller.js
+++ controllers/module-controller.js
@@ -17,15 +17,15 @@ router.post('/', async (req,res) => {
     }
 })
 
-router.put('/id', async (req,res) => {
+router.put('/:id', async (req,res) => {
     try {
-        res.json(await Module.findByIdAndUpdate(req.params.id, req.body, {new:true}))
+        res.json(await Module.findByIdAndUpdate(req.params.id, req.body, {new:true}));
     }catch (error){
         res.status(400).json(error);
     }
 })
 
-router.delete('id', async (req,res) => {
+router.delete('/:id', async (req,res) => {
     try {
         res.json(await Module.findByIdAndRemove(req.params.id));
     }catch(error){
